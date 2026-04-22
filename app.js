5e39cda2eec32c053d850b44f2364dd1fab87120
emmrants <emmrants@gmail.com>
2026-04-22 18:45:20 +0800

    fix LISTINGS global

--- app.js
+++ app.js
@@ -3,17 +3,11 @@ const SUPA_URL = 'https://bmphogbzsvpyvtqjxrvz.supabase.co'
 const SUPA_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImJtcGhvZ2J6c3ZweXZ0cWp4cnZ6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY4NDMzNDYsImV4cCI6MjA5MjQxOTM0Nn0.8NIs5zEaqqE8bWiUnsTvPlLU-pKbzxiN4YLbx6oAJVE'
 
 async function fetchListings() {
-
   const res = await fetch(`${SUPA_URL}/rest/v1/listings?select=*&order=created_at.desc`, {
-
     headers: { apikey: SUPA_KEY, Authorization: `Bearer ${SUPA_KEY}` }
-
   })
-
-  const data = await res.json()
-
-  renderListings(data)
-
+  LISTINGS = await res.json()
+  renderListings(LISTINGS)
 }
 
 async function postListing(body) {
